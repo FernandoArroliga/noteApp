8da3b0903c7754ecaa0218a292b6a7aed7ffa9d5
FernandoArroliga <FernandoArroliga@hotmail.com>
2023-12-11 00:13:45 -0600

    "Add javascript file"

--- main.js
+++ main.js
@@ -3,10 +3,23 @@ const notesContainer = document.querySelector(".notes-container");
 const createBtn = document.querySelector(".btn");
 let notes = document.querySelectorAll(".input-box");
 
+function showNotes(){
+    notesContainer.innerHTML = localStorage.getItem("notes");
+}
+
+showNotes();
+
+// Function for Store Data in the Browser
+function updateStorage(){
+    localStorage.setItem("notes", notesContainer.innerHTML);
+}
+
 // Event for Creating Notes
 createBtn.addEventListener("click", ()=>{
+
     let inputBox = document.createElement("p");
     let img = document.createElement("img");
+    
     inputBox.className = "input-box";
     inputBox.setAttribute("contenteditable", "true");
     img.src = "images/delete.png";
@@ -15,7 +28,27 @@ createBtn.addEventListener("click", ()=>{
 
 // Event for Deleting Notes
 notesContainer.addEventListener("click", function(e){
+    
     if (e.target.tagName === "IMG"){
         e.target.parentElement.remove();
+
+        // Update data in the browser
+        updateStorage();
+    }
+    else if(e.target.tagName === "p"){
+        notes = document.querySelectorAll(".input-box");
+        notes.forEach(nt => {
+            nt.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
+
+// Add enter key new line
+document.addEventListener("keydown", event => {
+    if(event.key === "Enter"){
+        document.execCommand("insertLineBreak");
+        event.preventDefault();
     }
 })
